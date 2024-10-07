bd7fe6a3c978b9facc7bac7516888fe83bb884c5
Alejandro Habernaud <45469431+th3l4stl0st@users.noreply.github.com>
2024-10-07 21:44:20 +0200

    forced vertical orientation

--- flutter_wordle/lib/app/app.dart
+++ flutter_wordle/lib/app/app.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_wordle/wordle/wordle.dart';
+import 'package:flutter/services.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+      ]);
     return MaterialApp(
       title: 'Flutter Wordle App',
       debugShowCheckedModeBanner: false,
